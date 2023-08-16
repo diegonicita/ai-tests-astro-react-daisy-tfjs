afc8c7143c588a7fb923d941ee59d28fca06d781
diegonicita <diegonicita@gmail.com>
2023-08-16 00:08:12 -0300

    add mdx styles and change triquinosis to mdx

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,4 +1,6 @@
 /** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   content: [
     './src/layouts/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
@@ -8,7 +10,44 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require('daisyui')],
+  plugins: [    
+    plugin(function ({ addBase, theme }) {
+      addBase({
+        h1: {
+          fontSize: theme('fontSize.2xl'),
+          marginTop: theme('spacing.4'),                              
+        },
+        h2: {
+          fontSize: theme('fontSize.xl'),
+          marginTop: theme('spacing.4'),                           
+        },
+        h3: {
+          fontSize: theme('fontSize.lg'),
+          marginTop: theme('spacing.4'),                                        
+        },
+        p: {
+          marginTop: theme('spacing.2'),
+          lineHeight: theme('lineHeight.7'),          
+        },
+        ul: {
+          marginTop: theme('spacing.4'),                              
+          listStyleType: 'disc',
+          marginLeft: theme('spacing.4'),
+        },
+        ol: {
+          marginTop: theme('spacing.4'),                              
+          listStyleType: 'decimal',
+          marginLeft: theme('spacing.4'),
+        },
+        li: {
+          marginBottom: theme('spacing.2'),
+        },
+        '.markdown p + p': {
+          marginTop: theme('spacing.4'), 
+        },
+      })
+    }),
+    require('daisyui')],
   daisyui: {
     themes: ['light', 'dark', 'cupcake', 'cyberpunk'],
   },
