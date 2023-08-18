b84af4f791c2cdc77446adb404621101d865f34c
diegonicita <diegonicita@gmail.com>
2023-08-18 15:59:12 -0300

    Font menu changes font family

--- src/components/navbar/js/fonts.js
+++ src/components/navbar/js/fonts.js
@@ -1,9 +1,12 @@
 import { fontAtom } from '../../../store/store'
 
 export function setFontButtonsListeners() {
-  function setFont(e) {        
+  function setFont(e) {
     fontAtom.set(e.target.name)
-    console.log("clic: " + e.target.name)
+    console.log('clic: ' + e.target.name)
+    var bodyElement = document.body
+    bodyElement.classList.remove(bodyElement.classList.item(0))
+    bodyElement.classList.add("font" + "-" + e.target.name)
   }
   const buttons = document.querySelectorAll('button.font')
   buttons.forEach((button) => {
