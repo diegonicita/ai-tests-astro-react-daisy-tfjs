67baafbba23cb35f6a15316a77bd9469c2c83b42
diegonicita <diegonicita@gmail.com>
2023-08-18 23:02:23 -0300

    Add more fonts

--- src/components/navbar/js/fonts.js
+++ src/components/navbar/js/fonts.js
@@ -5,8 +5,11 @@ export function setFontButtonsListeners() {
     fontAtom.set(e.target.name)
     console.log('clic: ' + e.target.name)
     var bodyElement = document.body
-    bodyElement.classList.remove(bodyElement.classList.item(0))
-    bodyElement.classList.add("font" + "-" + e.target.name)
+    while (bodyElement.classList.length > 0) {
+      bodyElement.classList.remove(bodyElement.classList.item(0))
+    }
+    bodyElement.classList.add(e.target.name)
+    console.log(bodyElement.classList)
   }
   const buttons = document.querySelectorAll('button.font')
   buttons.forEach((button) => {
