ca1890dc8ed2bd692c39963814cd4ca139b73933
diegonicita <diegonicita@gmail.com>
2023-09-05 11:34:19 -0300

    Change public folder structure and apps navigation

--- src/components/cards/choices/cardJSX.jsx
+++ src/components/cards/choices/cardJSX.jsx
@@ -1,3 +1,5 @@
+import { Option } from './option'
+
 export const CardJSX = ({
   title,
   description,
@@ -9,20 +11,18 @@ export const CardJSX = ({
   opcion4,
   opcion5,
 }) => (
-  <div className="card bg-base-100 shadow-lg shadow-stone-400 my-4 max-w-4xl text-xl">
-    <div className="card-body items-center">
-      <h2 className="card-title">{title}</h2><div>Año: {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}</div>
-      <div className="w-full">{description}</div>      
-      <div className="mt-4 card-actions flex-col items-center">
-        <div className="btn btn-outline w-full h-fit  text-lg">1) {opcion1}</div>
-        <div className="btn btn-outline w-full h-fit text-lg">2) {opcion2}</div>
-        <div className="btn btn-outline w-full h-fit text-lg">3) {opcion3}</div>
-        <div className="btn btn-outline w-full h-fit text-lg">4) {opcion4}</div>        
-        {opcion5 && (
-          <div className="btn btn-outline w-full h-fit text-lg">
-            5) {opcion5}
-          </div>
-        )}
+  <div className="my-4 text-xl">
+    <div className="card-body p-0">
+      <h2 className="card-title">
+        {title} - Año {price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, '.')}
+      </h2>
+      <div className="w-full">{description}</div>
+      <div className="mt-4 card-actions m-0">
+        <Option numero="1" texto={opcion1} />
+        <Option numero="2" texto={opcion2} />
+        <Option numero="3" texto={opcion3} />
+        <Option numero="4" texto={opcion4} />
+        {opcion5 && <Option numero="5" texto={opcion5} />}
       </div>
     </div>
   </div>
---
+++ src/components/cards/choices/option.jsx
@@ -0,0 +1,7 @@
+export const Option = ({ numero, texto }) => (
+  <>
+    <div className="btn btn-outline w-full h-fit text-lg text-start justify-start">
+      {numero}) {texto}
+    </div>
+  </>
+)
