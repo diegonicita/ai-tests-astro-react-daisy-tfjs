78c9cc1a72abc23ddfa3f7906ab24626c324f71c
diegonicita <diegonicita@gmail.com>
2023-09-06 15:27:23 -0300

    Add step and more tests

--- src/components/cards/choices/cardsJSX.jsx
+++ src/components/cards/choices/cardsJSX.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react'
 import { CardJSX } from './cardJSX'
 
-export const CardsJSX = ({ limit, year, id }) => {
-  const [data, setData] = useState(null)  
+export const CardsJSX = ({ limit, year, id, desde }) => {
+  const [data, setData] = useState(null)
 
   useEffect(() => {
     // Realizar la solicitud a la API usando fetch
@@ -12,7 +12,9 @@ export const CardsJSX = ({ limit, year, id }) => {
         '&ano=' +
         year +
         '&examen=' +
-        id,
+        id +
+        '&desde=' +
+        desde,
     )
       .then((response) => response.json())
       .then((data) => {
@@ -25,11 +27,23 @@ export const CardsJSX = ({ limit, year, id }) => {
 
   return (
     <>
+      <ul className="steps mt-4">
+        <li data-content="1" className="step step-info"></li>
+        <li data-content="11" className="step"></li>
+        <li  data-content="21" className="step"></li>
+        <li  data-content="31"className="step"></li>
+        <li  data-content="41"className="step"></li>
+        <li  data-content="51"className="step"></li>
+        <li  data-content="61"className="step"></li>
+        <li  data-content="71"className="step"></li>
+        <li  data-content="81"className="step"></li>
+        <li  data-content="91"className="step"></li>
+      </ul>
       {data &&
         data.map((item, index) => (
           <CardJSX
             key={index}
-            title={'Pregunta: ' + item.numero}
+            title={'Pregunta N°' + item.numero}
             description={item.texto}
             image={item.imagen}
             year={item.ano}
