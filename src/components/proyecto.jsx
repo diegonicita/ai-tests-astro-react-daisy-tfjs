064cee69979176a4a37d5a76664edc7e8ff035f2
diegonicita <diegonicita@gmail.com>
2023-11-03 15:28:26 -0300

    Add change to my CV

--- src/components/proyecto.jsx
+++ src/components/proyecto.jsx
@@ -1,33 +1,32 @@
 import { useStore } from '@nanostores/react'
 import { languageAtom } from '../store/store'
 
-export const Proyecto = ({ title, tools, link, esp = '', por = '' }) => {
+export const Proyecto = ({ image, title, tools, link, esp = '', por = '' }) => {
   const $languageAtom = useStore(languageAtom)
+  
   return (
     <>
-      <div class="flex flex-row">
-        <div class="avatar justify-center items-center">
-          <div class="self-start mt-9 w-24 md:w-2/3 md:self-center md:mt-0">
-            <figure>
-              <img src="/webapp/logo.png" alt="logo" />
-            </figure>
-          </div>
-        </div>
-        <div class="flex flex-column flex-1">
-          <div>
-            <h3>
-              <span class="font-bold">{title}:</span>
+      <div className="flex sm:flex-row flex-col ">
+        <div className="justify-start items-center flex flex-col">          
+        <h3 className="mt-0 max-w-full sm:max-w-[8rem] text-center">
+              <span className="font-bold mt-0 max-w-full sm:max-w-[8rem]">{title}</span>
             </h3>
+            <figure className="max-w-full sm:max-w-[8rem] h-auto px-4 mb-8">
+              <img className="rounded" src={image?image:"/webapp/logo.png"} alt="vetcare"/>
+            </figure>          
+        </div>
+        <div className="flex flex-column flex-1">
+          <div>            
             {$languageAtom == 'espaniol' && esp}
             {$languageAtom == 'portugues' && por}
-            <div class="mt-2 mb-8">
-              <span class="font-bold">Tech Stack: </span>
+            <div className="mt-2 mb-8">
+              <span className="font-bold">Tech Stack: </span>
               {tools}
               {link && (
-                <div>
+                <div className="mt-2">
                   <>
-                    <span class="font-bold">Link: </span>
-                    <a class="underline underline-offset-2" href={link}>
+                    <span className="font-bold">Link: </span>
+                    <a className="underline underline-offset-2" href={link}>
                       {title}
                     </a>
                   </>
