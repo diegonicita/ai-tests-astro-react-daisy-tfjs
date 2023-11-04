527f8d036f6078743ddaf93ac2f78866dac6c1e6
diegonicita <diegonicita@gmail.com>
2023-11-04 17:00:10 -0300

    Fix cards styles in Mercado

--- src/components/cards/cardJSX.jsx
+++ src/components/cards/cardJSX.jsx
@@ -1,10 +1,10 @@
 export const CardJSX = ({ title, description, image, price }) => (
-  <div className="card text-sm w-full lg:w-70 bg-base-100 shadow-lg p-2 hover:shadow-2xl transition duration-300 ease px-0 pt-2 hover:cursor-pointer">
-    <figure className="pt-4">
+  <div className="card text-sm w-full lg:w-70 bg-base-100 shadow-lg p-2 hover:shadow-2xl transition duration-300 ease px-0 pt-2 hover:cursor-pointer  brightness-95">
+    <figure className="px-2">
       <img
         src={'https://mercado.webapp.ar/images/' + image}
         alt="Shoes"
-        className="w-full brightness-90 hover:brightness-95"
+        className="w-full brightness-90 hover:brightness-95 rounded-xl"
       />
     </figure>
     <div className="card-body items-center text-center p-0">
